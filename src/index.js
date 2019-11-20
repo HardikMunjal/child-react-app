15111d298a959e25795c7824a701d332a0bf510d
Hardi <hardik.munjal@pearson.com>
2019-11-20 14:13:14 +0530

    updated hello world file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// HelloWorld.js
+import React from 'react'
+const HelloWorld = (props) => {
+  const children = props.children
+    ? <span>{props.children}</span>
+    : false
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      {children}
+    </div>
+  )
+}
+export default HelloWorld
